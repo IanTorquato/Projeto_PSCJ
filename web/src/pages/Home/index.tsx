956e466cbaf15f4b2be5e6cdf6229d5135facb31
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-06-23 00:32:51 -0300

    Tratando a página inicial

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -5,19 +5,57 @@ import logo from '../../assets/logo.webp'
 
 const Home = () => {
     return (
-        <div>
-            <div id="imgFundo">
-                <div id="centralInfo">
-                    <img id="logo" src={logo} alt="Brasão da Paróquia" />
+        <div className="imgFundo">
+            <div className="telaInicial">
+                <div className="centralInfo">
+                    <img className="logo" src={logo} alt="Brasão da Paróquia" />
                     <hr className="linha" />
 
-                    <h1 id="titleSCJ">Santuário Sagrado Coração de Jesus</h1>
+                    <h1 className="tituloSCJ">Santuário Sagrado Coração de Jesus</h1>
 
                     <hr className="linha" />
-                    <h2 id="semiTitleSCJ">Gravatal - SC</h2>
+                    <h2 className="subTituloSCJ">Gravatal - SC</h2>
                 </div>
             </div>
-            <div id="teste"><p>Apenas um Teste!</p></div>
+
+            <div className="divBotao">
+                <div className="btnCadastrar">Cadastrar Missa</div>
+            </div>
+
+            <div className="cronograma">
+                <h1 className="proximasMissas">PRÓXIMAS MISSAS</h1>
+
+                <div className="doisItems">
+                    <div className="detalhesMissa">
+                        <h1 className="tituloMissa">23/06 - 19:30</h1>
+
+                        <h2 className="subTituloMissa">SEGUNDA-FEIRA | CAPELA SANTO ANTÔNIO - TERMAS</h2>
+                    </div>
+
+                    <div className="detalhesMissa">
+                        <h1 className="tituloMissa">26/06 - 19:30</h1>
+
+                        <h2 className="subTituloMissa">SEXTA-FEIRA | SANTUÁRIO SCJ - CENTRO</h2>
+                    </div>
+                </div>
+
+                <div className="doisItems">
+                    <div className="detalhesMissa">
+                        <h1 className="tituloMissa">27/06 - 19:30</h1>
+
+                        <h2 className="subTituloMissa">SÁBADO | SANTUÁRIO SCJ - CENTRO</h2>
+                    </div>
+
+                    <div className="detalhesMissa">
+                        <h1 className="tituloMissa">28/06 - 19:00</h1>
+
+                        <h2 className="subTituloMissa">DOMINGO | SANTUÁRIO SCJ - CENTRO</h2>
+                    </div>
+                </div>
+            </div>
+
+            <footer className="cabecalho">
+            </footer>
         </div>
     )
 }
