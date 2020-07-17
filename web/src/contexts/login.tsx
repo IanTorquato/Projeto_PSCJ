d529b02e3496a5a8c8cf8da13e67ca610b0c7ef3
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-07-17 15:39:58 -0300

    Parte de Login no front
    Não entendo totalmente os códigos aqui usados, pois foi meu primeiro login e tomei como base um vídeo da Rocketseat para me ajudar.
    Pretendo rever os conceitos e também estudar outras formas de login assim que der.

--- web/src/contexts/login.tsx
+++ web/src/contexts/login.tsx
@@ -1,39 +1,53 @@
 import React, { createContext, useState, useEffect } from 'react'
 
-import { login } from '../services/login'
+import api from '../services/api'
+
+interface User {
+  usuario: string
+  senha: string
+}
 
 interface LoginContextData {
   logado: boolean
-  user: object | null
+  userr: object | null
   loading: boolean
-  logar(): Promise<void>
+  logar(user: User): Promise<void>
 }
 
 const LoginContext = createContext<LoginContextData>({} as LoginContextData)
 
 export const LoginProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState<object | null>(null)
+  const [userr, setUserr] = useState<object | null>(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const storagedUser = localStorage.getItem('@PSCJ:user')
 
     if (storagedUser) {
-      setUser(JSON.parse(storagedUser))
+      setUserr(JSON.parse(storagedUser))
     }
     setLoading(false)
   }, [])
 
-  async function logar() {
-    const response = await login()
+  async function logar(user: User) {
+    if (userr) {
+      console.log(userr)
+    }
+    else {
+      const response = await api.get(`/login/${user.usuario}/${user.senha}`)
 
-    setUser(response.user)
-
-    localStorage.setItem('@PSCJ:user', JSON.stringify(response.user))
+      if (response.data.user) {
+        localStorage.setItem('@PSCJ:user', JSON.stringify(response.data.user))
+        setUserr(response.data)
+      }
+      else {
+        alert(response.data.erro)
+      }
+    }
   }
 
   return (
-    <LoginContext.Provider value={{ logado: !!user, user, loading, logar }}>
+    <LoginContext.Provider value={{ logado: !!userr, userr, loading, logar }}>
       {children}
     </LoginContext.Provider>
   )
--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,23 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext, FormEvent } from 'react'
 
 import './styles.css'
-import Footer from '../../components/Footer'
 import LoginContext from '../../contexts/login'
+import logo from '../../assets/logo.webp'
 
 const Login = () => {
   const { logar } = useContext(LoginContext)
 
-  async function handleLogin() {
-    logar()
+  async function entrar(event: FormEvent) {
+    event.preventDefault()
+    const usuario = document.querySelector<HTMLInputElement>('input#usuario')?.value
+    const senha = document.querySelector<HTMLInputElement>('input#senha')?.value
+
+    if (usuario && senha) {
+      const user = { usuario, senha }
+      logar(user)
+    }
   }
 
   return (
-    <>
-      <section className="secLogin">
-        <input type="button" value="Logar" className="btnLogar" onClick={handleLogin} />
-      </section>
-      <Footer />
-    </>
+    <section className="secLogin">
+      <form onSubmit={entrar}>
+        <img src={logo} alt="Brasão da Paróquia" />
+        <div className="insereDados">
+          <label htmlFor="usuario">Usuário:</label>
+          <input type="text" id="usuario" placeholder="Insira o usuário" />
+        </div>
+        <div className="insereDados">
+          <label htmlFor="senha">Senha:</label>
+          <input type="text" id="senha" placeholder="Insira a senha" />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </section>
   )
 }
 
