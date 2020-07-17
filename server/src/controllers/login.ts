0634d136b94e44e851e7703be20e9bd00771d13d
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-07-17 18:13:12 -0300

    Criptografia básica na senha
    Apenas para não ser possível visualizar a senha pelo Armazenamento Local.

--- server/src/controllers/login.ts
+++ server/src/controllers/login.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
 
 class Login {
   async login(request: Request, response: Response) {
     const user = { usuario: 'ian', senha: 'ian' }
     const { usuario, senha } = request.body
 
-    if (user.usuario === usuario && user.senha === senha) {
-      return response.json({ user })
+    const senhaEncriptada = bcrypt.hashSync(senha, 10)
+
+    if (user.usuario === usuario && bcrypt.compareSync(user.senha, senhaEncriptada)) {
+      return response.json({ user: { usuario, senha: senhaEncriptada } })
     }
     else {
       return response.json({ erro: 'Verifique os dados e tente novamente!' })
