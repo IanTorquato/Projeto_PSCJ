566ed277055c9a00f9ba0946a257457a17d7c763
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-07-20 19:59:42 -0300

    StatusBar e outros detalhes

--- mobile/src/pages/CadastrarUsuario/index.tsx
+++ mobile/src/pages/CadastrarUsuario/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, ImageBackground, Image, TextInput, UIManager } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ImageBackground, Image, TextInput } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler'
 import { FontAwesome5 as Fa } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
@@ -8,97 +8,97 @@ const fundoLogin = require('../../assets/fundoLogin.jpg')
 const logo = require('../../../assets/icon.png')
 
 const CadastrarUsuario = () => {
-    const navigation = useNavigation()
+	const navigation = useNavigation()
 
-    function handleNavigateBack() {
-        navigation.goBack()
-    }
+	function handleNavigateBack() {
+		navigation.goBack()
+	}
 
-    return (
-        <View style={styles.container}>
-            <ImageBackground source={fundoLogin} style={styles.fundoLogin}>
-                <Image source={logo} style={styles.imgLogo} />
+	return (
+		<View style={styles.container}>
+			<ImageBackground source={fundoLogin} style={styles.fundoLogin}>
+				<Image source={logo} style={styles.imgLogo} />
 
-                <TouchableOpacity onPress={handleNavigateBack} style={styles.btnVoltar}>
-                    <Fa name="arrow-circle-left" color="#fff" size={32} />
-                </TouchableOpacity>
+				<TouchableOpacity onPress={handleNavigateBack} style={styles.btnVoltar}>
+					<Fa name="arrow-circle-left" color="#fff" size={32} />
+				</TouchableOpacity>
 
-                <View style={styles.containerInputs}>
-                    <Text style={styles.txtInput}>Nome:</Text>
-                    <TextInput style={styles.input} placeholder="Digite seu nome" />
-                </View>
+				<View style={styles.containerInputs}>
+					<Text style={styles.txtInput}>Nome:</Text>
+					<TextInput style={styles.input} placeholder="Digite seu nome" />
+				</View>
 
-                <View style={styles.containerInputs}>
-                    <Text style={styles.txtInput}>E-mail:</Text>
-                    <TextInput style={styles.input} placeholder="Digite seu e-mail" />
-                </View>
+				<View style={styles.containerInputs}>
+					<Text style={styles.txtInput}>E-mail:</Text>
+					<TextInput style={styles.input} placeholder="Digite seu e-mail" />
+				</View>
 
-                <RectButton style={styles.botao}>
-                    <Text style={styles.txtBotao}>Cadastrar-se</Text>
-                </RectButton>
-            </ImageBackground>
-        </View>
-    );
+				<RectButton style={styles.botao}>
+					<Text style={styles.txtBotao}>Cadastrar-se</Text>
+				</RectButton>
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
+	container: {
+		flex: 1
+	},
 
-    fundoLogin: {
-        flex: 1,
-        resizeMode: 'cover',
-        alignItems: 'center',
-    },
+	fundoLogin: {
+		flex: 1,
+		alignItems: 'center'
+	},
 
-    imgLogo: {
-        width: 130,
-        top: '10%',
-        resizeMode: 'contain'
-    },
+	btnVoltar: {
+		position: 'absolute',
+		left: 16,
+		top: 16
+	},
 
-    btnVoltar: {
-        top: '-20%',
-        left: '-40%'
-    },
+	imgLogo: {
+		width: 130,
+		top: 75,
+		resizeMode: 'contain'
+	},
 
-    containerInputs: {
-        top: '10%',
-        width: '80%',
-        marginBottom: 15
-    },
+	containerInputs: {
+		top: 60,
+		width: 280,
+		marginBottom: 15
+	},
 
-    txtInput: {
-        color: '#fff',
-        left: 10,
-        marginBottom: 5,
-        fontSize: 16
-    },
+	txtInput: {
+		color: '#fff',
+		left: 10,
+		marginBottom: 5,
+		fontSize: 16
+	},
 
-    input: {
-        backgroundColor: '#fff',
-        color: '#000',
-        borderRadius: 30,
-        height: 50,
-        fontSize: 20,
-        paddingLeft: 15
-    },
+	input: {
+		backgroundColor: '#fff',
+		color: '#000',
+		borderRadius: 30,
+		height: 50,
+		fontSize: 20,
+		paddingLeft: 15
+	},
 
-    botao: {
-        top: '20%',
-        height: 55,
-        backgroundColor: '#e41e25',
-        justifyContent: 'center',
-        borderRadius: 30,
-        paddingHorizontal: 25
-    },
+	botao: {
+		top: 100,
+		height: 55,
+		backgroundColor: '#e41e25',
+		justifyContent: 'center',
+		borderRadius: 30,
+		paddingHorizontal: 25
+	},
 
-    txtBotao: {
-        color: '#fff',
-        fontSize: 24,
-        top: -2
-    }
+	txtBotao: {
+		color: '#fff',
+		fontSize: 24,
+		top: -2
+	}
 });
 
 export default CadastrarUsuario
