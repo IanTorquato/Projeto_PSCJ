6cb8bdb28224f0621db104aa7d3fa78883ff04d9
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-07-29 14:58:23 -0300

    ROta de Cadastro de usuários
    Uso de YUP e do Alert do react native

--- mobile/src/pages/CadastrarUsuario/index.tsx
+++ mobile/src/pages/CadastrarUsuario/index.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, ImageBackground, Image, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, ImageBackground, Image, TextInput, Alert } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler'
 import { FontAwesome5 as Fa } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
+import * as Yup from 'yup'
+
+import { useContextLogin } from '../../contexts/login'
+import api from '../../services/api'
 
 const fundoLogin = require('../../assets/fundoLogin.jpg')
 const logo = require('../../../assets/icon.png')
 
 const CadastrarUsuario = () => {
+	const [nome, setNome] = useState('')
+	const [email, setEmail] = useState('')
+
 	const navigation = useNavigation()
+	const { logar } = useContextLogin()
 
 	function handleNavigateBack() {
 		navigation.goBack()
+	}
+
+	async function cadastrar() {
+		try {
+			const schemaDadosCadastro = Yup.object().shape({
+				nome: Yup.string().trim().required('O campo Nome é obrigatório!').min(3, 'O nome deve conter ao menos 3 caracteres!'),
+				email: Yup.string().trim().required('O campo E-mail é obrigatório!').email('Digite um E-mail válido!')
+			})
+
+			const usuario = await schemaDadosCadastro.validate({ nome, email })
+			const { data } = await api.post('usuarios', usuario)
+
+			if (data.erro) {
+				Alert.alert('Erro', data.erro)
+			} else {
+				// Alert("Título", "Mensagem", [{Botões}], {Opções})
+				Alert.alert('Sucesso', data.mensagem,
+					[{ text: "Logar", onPress: () => logar({ id: 0, nome, email }) }], { cancelable: false }
+				)
+			}
+		} catch (erro) {
+			if (erro instanceof Yup.ValidationError) {
+				Alert.alert('Erro', erro.errors[0])
+			}
+			else {
+				Alert.alert('Erro', erro)
+			}
+		}
 	}
 
 	return (
@@ -25,15 +61,17 @@ const CadastrarUsuario = () => {
 
 				<View style={styles.containerInputs}>
 					<Text style={styles.txtInput}>Nome:</Text>
-					<TextInput style={styles.input} placeholder="Digite seu nome" />
+					<TextInput style={styles.input} placeholder="Digite seu nome"
+						onChange={event => setNome(event.nativeEvent.text)} />
 				</View>
 
 				<View style={styles.containerInputs}>
 					<Text style={styles.txtInput}>E-mail:</Text>
-					<TextInput style={styles.input} placeholder="Digite seu e-mail" />
+					<TextInput style={styles.input} placeholder="Digite seu e-mail"
+						onChange={event => setEmail(event.nativeEvent.text)} />
 				</View>
 
-				<RectButton style={styles.botao}>
+				<RectButton style={styles.botao} onPress={cadastrar} >
 					<Text style={styles.txtBotao}>Cadastrar-se</Text>
 				</RectButton>
 			</ImageBackground>
