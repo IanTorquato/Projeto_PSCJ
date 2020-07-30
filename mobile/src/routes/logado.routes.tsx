67090dae9bd42625a63348230b177f0c4eb2c685
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-07-30 19:38:05 -0300

    Criação da navegação Tab do app

--- mobile/src/routes/logado.routes.tsx
+++ mobile/src/routes/logado.routes.tsx
@@ -1,19 +1,45 @@
 import React from 'react'
+import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
 import Home from '../pages/Home'
 
-const AppStack = createStackNavigator()
+const App = createBottomTabNavigator()
 
 const RoutesLogado = () => {
 	return (
 		<NavigationContainer>
-			<AppStack.Navigator headerMode="none"
-				screenOptions={{ cardStyle: { backgroundColor: '#000' } }}>
+			<App.Navigator screenOptions={({ route }) => ({
+				tabBarIcon: ({ color, size }) => {
+					let iconName = 'ios-list'
 
-				<AppStack.Screen name="Home" component={Home} />
-			</AppStack.Navigator>
+					if (route.name === 'Home') {
+						iconName = 'home'
+						size = 28
+					}
+					else if (route.name === 'Perfil') {
+						iconName = 'user-alt'
+						size = 22
+					}
+					else if (route.name === 'Missas') { iconName = 'list-ul' }
+
+					return <FontAwesome5 name={iconName} size={size} color={color} />
+				},
+			})}
+				tabBarOptions={{
+					activeTintColor: '#eee',
+					inactiveTintColor: 'gray',
+					labelStyle: { fontSize: 12, marginBottom: 4, marginTop: -4 },
+					tabStyle: { backgroundColor: '#111', marginTop: -1 }, style: { height: 56 }
+				}}
+			>
+
+				<App.Screen name="Home" component={Home} />
+				<App.Screen name="Missas" component={Home} />
+				<App.Screen name="Perfil" component={Home} />
+			</App.Navigator>
 		</NavigationContainer>
 	)
 }
