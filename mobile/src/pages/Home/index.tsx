a452a76889da0515883586ab804671a42e637129
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-08-12 18:58:41 -0300

    Diminuindo 4 linhas de código

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -16,11 +16,7 @@ const Home = () => {
       <ScrollView>
         <Svg width="100%" height={360} viewBox="0 0 500 500" fill="none">
           <G>
-            <Path
-              d="M0 0h500v280.095a700.272 700.272 0 01-492.576 2.743L0 276.095V0z"
-              fill="#710b0e"
-              fillOpacity={0.8}
-            />
+            <Path d="M0 0h500v280.095a700.272 700.272 0 01-492.576 2.743L0 276.095V0z" fill="#710b0e" fillOpacity={0.8} />
           </G>
 
           <View style={styles.viewTopoVermelho}>
