f2ced3e676012bafc9353ec1d48a0ebc44fe08f2
Ian Torquato <iantorquato2@gmail.com>
2021-01-06 18:57:42 -0300

    Mantendo tela de Splash até carregar as fontes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { StatusBar } from 'react-native'
-import { AppLoading } from 'expo'
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen'
 import { useFonts, Cinzel_400Regular } from '@expo-google-fonts/cinzel'
 import { Cookie_400Regular } from '@expo-google-fonts/cookie'
 import { Lora_400Regular } from '@expo-google-fonts/lora'
@@ -10,13 +10,15 @@ import Routes from './src/routes'
 import { LoginProvider } from './src/contexts/login'
 
 export default function App() {
+	useEffect(() => {
+		async () => await preventAutoHideAsync().catch(error => console.log(error))
+	}, [])
+
 	const [fontsLoaded, error] = useFonts({ Cinzel_400Regular, Cookie_400Regular, Lora_400Regular, Roboto_400Regular })
 
-	if (!fontsLoaded) {
-		return <AppLoading />
-	}
+	error && console.log(error)
 
-	return (
+	return fontsLoaded && hideAsync() && (
 		<LoginProvider>
 			<StatusBar barStyle="light-content" backgroundColor="#000" />
 			<Routes />
