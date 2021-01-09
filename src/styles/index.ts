fd32be8e2218d4ce26ee717c7586174f5c894200
Ian Torquato <iantorquato2@gmail.com>
2021-01-08 21:09:47 -0300

    Arquivo com cores e fontees globais

---
+++ src/styles/index.ts
@@ -0,0 +1,56 @@
+const cores = {
+	principais: {
+		vermelhoClaro: '#e41325',
+		vermelhoNormal: '#960d18',
+		vermelhoEscuro: '#5e080f',
+		amareloClaro: '#ffc32c',
+		amareloNormal: '#cc9200',
+		amareloEscuro: '#a37500',
+		amareloMuitoEscuro: '#7a5800',
+		azulClaro: '#4396df',
+		azulNormal: '#1e6bae',
+		azulEscuro: '#0f3557',
+		azulMuitoEscuro: '#071b2c'
+	},
+	neutras: {
+		preto: '#121212',
+		cinzaMuitoEscuro: '#252525',
+		cinzaEscuro: '#747474',
+		cinzaNormal: '#c3c3c3',
+		cinzaClaro: '#e5e5e5',
+		branco: '#ffffff'
+	},
+	redesSociais: {
+		facebook: '#1877f2',
+		github: '#000000',
+		gmail: '#ff0000',
+		instagram: '#ec5cbe',
+		linkedin: '#1877f2',
+		whatsapp: '#00e676',
+		youtube: '#ff0000'
+	}
+}
+
+const fontes = {
+	cookie: {
+		fontFamily: 'Cookie_400Regular'
+	},
+	cormorant: {
+		fontFamily: 'CormorantGaramond_400Regular'
+	},
+	merriweather: {
+		fontFamily: 'Merriweather_400Regular'
+	},
+	oswald: {
+		fontFamily: 'Oswald_400Regular'
+	},
+	roboto: {
+		fontFamily: 'Roboto_400Regular'
+	},
+	shadows: {
+		fontFamily: 'ShadowsIntoLightTwo_400Regular',
+		letterSpacing: '8%'
+	}
+}
+
+export { cores, fontes }
