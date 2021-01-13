3886ffaf2583d22d9b2de35956d52139c9d6c901
Ian Torquato <iantorquato2@gmail.com>
2021-01-13 18:58:18 -0300

    Removendo navegação em tabs

--- src/routes/logado.routes.tsx
+++ src/routes/logado.routes.tsx
@@ -1,44 +1,23 @@
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { FontAwesome5 } from '@expo/vector-icons'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import Home from '../pages/Home'
-import RotasMissas from './missas.routes'
-import RotasPerfil from './perfil.routes'
+import Missas from '../pages/Missas'
+import DetalhesMissa from '../pages/DetalhesMissa'
+import Perfil from '../pages/Perfil'
+
 import { cores } from '../styles'
 
 const RoutesLogado = () => {
-	const { Navigator, Screen } = createBottomTabNavigator()
+	const { Navigator, Screen } = createStackNavigator()
 	const { preto } = cores.neutras
 
 	return (
-		<Navigator sceneContainerStyle={{ backgroundColor: preto }} screenOptions={({ route }) => ({
-			tabBarIcon: ({ color, size }) => {
-				let iconName = 'ios-list'
-
-				if (route.name === 'Home') {
-					iconName = 'home'
-					size = 28
-				}
-				else if (route.name === 'RotasPerfil') {
-					iconName = 'user-alt'
-					size = 22
-				}
-				else if (route.name === 'RotasMissas') { iconName = 'list-ul' }
-
-				return <FontAwesome5 name={iconName} size={size} color={color} />
-			}
-		})}
-			tabBarOptions={{
-				activeTintColor: '#eee',
-				inactiveTintColor: 'gray',
-				labelStyle: { fontSize: 12, marginBottom: 4, marginTop: -4 },
-				tabStyle: { backgroundColor: '#111', marginTop: -1 }, style: { height: 56 }
-			}}
-		>
+		<Navigator headerMode="none" screenOptions={{ cardStyle: { backgroundColor: preto } }}>
 			<Screen name="Home" component={Home} />
-			<Screen name="RotasMissas" component={RotasMissas} options={{ tabBarLabel: 'Missas' }} />
-			<Screen name="RotasPerfil" component={RotasPerfil} options={{ tabBarLabel: 'Perfil' }} />
+			<Screen name="Missas" component={Missas} />
+			<Screen name="DetalhesMissa" component={DetalhesMissa} />
+			<Screen name="Perfil" component={Perfil} />
 		</Navigator>
 	)
 }
