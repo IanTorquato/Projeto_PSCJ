ad5ffa5da577b003b1b91cef757fd84d2d5b6027
Ian Torquato <iantorquato2@gmail.com>
2021-01-22 13:54:19 -0300

    Reformulando toda página Home (Incompleta ainda)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,136 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Alert, Image, ScrollView, Text, View } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { FontAwesome5 } from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import api from '../../services/api'
+import CabecalhoApp from '../../components/CabecalhoApp'
+import BotaoPrimario from '../../components/BotaoPrimario'
+import Missa from '../../utils/interfaces'
+import { formatDiaMesHora } from '../../utils/tratandoDatas'
+import retornaCorDoCicloTernario from '../../utils/cicloCores'
+import { cores } from '../../styles'
 
 import styles from './styles'
 
 const Home = () => {
+	const [missas, setMissas] = useState<Missa[]>([])
+	const { branco } = cores.neutras
+
+	useEffect(() => {
+		api.get('missas?quantidade_missas=6')
+			.then(({ data }) => setMissas(formatDiaMesHora(data)))
+			.catch(({ response }) => {
+				console.log(response)
+				Alert.alert(response?.data.erro || 'Falha ao listar missas.')
+			})
+	}, [])
+
 	return (
-		<View >
-		</View>
+		<ScrollView style={styles.viewHome} showsHorizontalScrollIndicator={true}>
+			<CabecalhoApp />
+
+			<View style={styles.viewTitulo}>
+				<Text style={styles.txtTitulosHome}>Próximas Missas</Text>
+				<Text style={styles.txtTeste}>Filtarar Lolicadades</Text>
+			</View>
+
+			{!missas[0] ? <Text style={styles.txtNomeMissa}>Nada de missas aqui!</Text> : (
+				<ScrollView style={styles.viewMissas} horizontal showsHorizontalScrollIndicator={false}>
+					{missas.map((missa, index) => (
+						<View style={{ ...styles.viewBorda, ...retornaCorDoCicloTernario(index, 'borderColor') }} key={missa.id}>
+							<RectButton style={styles.btnMissa}>
+								<Image source={{ uri: missa.local_url }} style={styles.imgLocal} />
+
+								<View style={styles.viewDadosMissa}>
+									<Text style={styles.txtNomeMissa}>{missa.nome}</Text>
+
+									<View style={styles.viewIconDados}>
+										<View style={styles.viewDeixarEmLinha}>
+											<FontAwesome5 name="calendar-alt" color={branco} />
+											<Text style={styles.txtDadosMissa}>{missa.data}</Text>
+										</View>
+										<View style={styles.viewDeixarEmLinha}>
+											<MaterialCommunityIcons name="clock" color={branco} />
+											<Text style={styles.txtDadosMissa}>{missa.hora}</Text>
+										</View>
+									</View>
+
+									<View style={styles.viewIconDados}>
+										<View style={styles.viewDeixarEmLinha}>
+											<FontAwesome5 name="map-marked-alt" color={branco} />
+											<Text style={styles.txtDadosMissa}>{missa.local_nome}</Text>
+										</View>
+										<View style={styles.viewDeixarEmLinha}>
+											<MaterialCommunityIcons name="account-group" color={branco} />
+											<Text style={styles.txtDadosMissa}>{missa.pessoas_cadastradas}/{missa.max_pessoas}</Text>
+										</View>
+									</View>
+								</View>
+							</RectButton>
+						</View>
+					))}
+				</ScrollView>)}
+
+			<View style={styles.viewListaMissas}>
+				<Text style={styles.txtListaMissas}>Confira aqui a lista completa de missas da nossa Paróquia!</Text>
+
+				<BotaoPrimario text="Lista de Missas" styleComplements={styles.btnListaMissas} />
+			</View>
+
+			<View>
+				<Text style={styles.txtTitulosHome}>Orientações para a Santa Missa</Text>
+
+				<View style={styles.viewOrientacao}>
+					<View style={{ ...styles.imgOrientacao, ...retornaCorDoCicloTernario(0, 'backgroundColor') }}>
+						<FontAwesome5 name="mask" color={branco} size={24} />
+					</View>
+
+					<Text style={styles.txtOrientacao}>É obrigatório o uso de máscara;</Text>
+				</View>
+
+				<View style={styles.viewOrientacao}>
+					<View style={{ ...styles.imgOrientacao, ...retornaCorDoCicloTernario(1, 'backgroundColor') }}>
+						<FontAwesome5 name="mask" color={branco} size={24} />
+					</View>
+
+					<Text style={styles.txtOrientacao}>Higienize as mãos com álcool em gel;</Text>
+				</View>
+
+				<View style={styles.viewOrientacao}>
+					<View style={{ ...styles.imgOrientacao, ...retornaCorDoCicloTernario(2, 'backgroundColor') }}>
+						<FontAwesome5 name="mask" color={branco} size={24} />
+					</View>
+
+					<Text style={styles.txtOrientacao}>Duas pessoas por banco, exceto familiares;</Text>
+				</View>
+
+				<View style={styles.viewOrientacao}>
+					<View style={{ ...styles.imgOrientacao, ...retornaCorDoCicloTernario(3, 'backgroundColor') }}>
+						<FontAwesome5 name="mask" color={branco} size={24} />
+					</View>
+
+					<Text style={styles.txtOrientacao}>Mantenha o distanciamento de 1,5m;</Text>
+				</View>
+
+				<View style={styles.viewOrientacao}>
+					<View style={{ ...styles.imgOrientacao, ...retornaCorDoCicloTernario(4, 'backgroundColor') }}>
+						<FontAwesome5 name="mask" color={branco} size={24} />
+					</View>
+
+					<Text style={styles.txtOrientacao}>Não ultrapasse o cordão de isolamento;</Text>
+				</View>
+
+				<View style={styles.viewOrientacao}>
+					<View style={{ ...styles.imgOrientacao, ...retornaCorDoCicloTernario(5, 'backgroundColor') }}>
+						<FontAwesome5 name="mask" color={branco} size={24} />
+					</View>
+
+					<Text style={styles.txtOrientacao}>As ofertas são feitas no fim da missa;</Text>
+				</View>
+			</View>
+		</ScrollView>
 	)
 }
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,139 @@
 import { StyleSheet } from 'react-native'
 
+import { cores, fontes } from '../../styles'
+
+const { amareloClaro } = cores.principais
+const { branco, cinzaMuitoEscuro } = cores.neutras
+
+const { cormorant, cormorantBold, merriweather, oswald, roboto, shadows } = fontes
+
 const styles = StyleSheet.create({
+	viewHome: {
+		paddingHorizontal: 16
+	},
+
+	viewTitulo: {
+		alignItems: 'center',
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+
+	txtTitulosHome: {
+		color: branco,
+		fontSize: 32,
+		lineHeight: 46,
+		...oswald,
+		marginBottom: 24
+	},
+
+	txtTeste: {
+		color: branco,
+		fontSize: 14,
+		paddingBottom: 24,
+		...roboto
+	},
+
+	viewMissas: {
+		marginHorizontal: -4
+	},
+
+	viewBorda: {
+		borderRadius: 4,
+		borderStyle: 'solid',
+		borderWidth: 1,
+		marginHorizontal: 4,
+	},
+
+	btnMissa: {
+		backgroundColor: cinzaMuitoEscuro,
+		borderRadius: 4,
+		height: 232,
+		width: 176
+	},
+
+	imgLocal: {
+		borderTopRightRadius: 4,
+		borderTopLeftRadius: 4,
+		height: 120,
+	},
+
+	viewDadosMissa: {
+		flex: 1,
+		paddingHorizontal: 8
+	},
+
+	txtNomeMissa: {
+		color: branco,
+		...cormorantBold,
+		fontSize: 16,
+		marginVertical: 8,
+		textAlign: 'center'
+	},
+
+	viewIconDados: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginBottom: 8,
+		marginHorizontal: 8
+	},
+
+	viewDeixarEmLinha: {
+		alignItems: 'center',
+		flexDirection: 'row'
+	},
+
+	txtDadosMissa: {
+		color: branco,
+		fontSize: 12,
+		marginLeft: 4,
+		...merriweather
+	},
+
+	viewListaMissas: {
+		backgroundColor: '#5566aaaa',
+		marginHorizontal: 40,
+		marginTop: 48,
+		position: 'relative'
+	},
+
+	txtListaMissas: {
+		backgroundColor: '#550615aa',
+		color: branco,
+		fontSize: 18,
+		maxWidth: 280,
+		...shadows,
+		textAlign: 'center'
+	},
+
+	btnListaMissas: {
+		backgroundColor: amareloClaro,
+		marginBottom: 56,
+		marginTop: 24,
+		width: 208
+	},
+
+	viewOrientacao: {
+		alignItems: 'center',
+		flexDirection: 'row',
+		marginVertical: 12
+	},
+
+	imgOrientacao: {
+		alignItems: 'center',
+		backgroundColor: '#e41325',
+		borderRadius: 100,
+		height: 40,
+		justifyContent: 'center',
+		padding: 4,
+		width: 40
+	},
+
+	txtOrientacao: {
+		color: branco,
+		...cormorant,
+		fontSize: 20,
+		marginLeft: 8
+	}
 })
 
 export default styles
